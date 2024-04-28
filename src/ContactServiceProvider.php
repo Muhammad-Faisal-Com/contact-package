1609057f2f42ca26723e6007c3c852c8f320b544
Muhammad-Faisal-Com <mfaisal@computan.net>
2024-04-28 18:22:48 +0500

    first commit

--- src/ContactServiceProvider.php
+++ src/ContactServiceProvider.php
@@ -7,11 +7,14 @@ class ContactServiceProvider extends ServiceProvider
     public function boot(){
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views/','contact');
-        $this->loadMigrationsFrom(__DIR__.'/database/migration/');
+        $this->loadMigrationsFrom(__DIR__.'/database/Migration/');
 //        $this->mergeConfigFrom(__DIR__.'/config/', 'contact');
 //        $this->publishes([
 //
 //        ]);
+        $this->publishes([
+            __DIR__ . '/database/Migration/2024_04_28_085840_create_contacts_table.php' => database_path('migrations/2024_04_28_085840s_create_contacts_table.php_create_Contact_table.php')
+        ], 'migration');
     }
     public function register(){
 
